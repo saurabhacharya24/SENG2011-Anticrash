b2387bf92a4d8b717122614453f894b074b52a06
saurabhacharya24 <saurabhacharya123@gmail.com>
2019-10-29 18:18:25 +1100

    Fixed autoincrement problems, populated donor table

--- database/schema.sql
+++ database/schema.sql
@@ -8,19 +8,17 @@ create table donors (
     contact_info string,
     blood_type string,
     date_last_donated Date,
-    donor_id integer auto_increment,
-    primary key (donor_id)
+    donor_id integer primary key autoincrement
 );
 
 
 create table donor_samples (
-    sample_id integer auto_increment,
+    sample_id integer primary key autoincrement,
     donor_id integer,
     blood_type string,
     location_of_donation string,
     use_by_date Date,
     abnormalities boolean,
-    primary key (sample_id),
     foreign key (donor_id) references donors(donor_id),
     foreign key (blood_type) references donors(blood_type)
 );
@@ -39,9 +37,8 @@ create table requests (
     blood_type string,
     quantity_needed integer,
     date_of_request Date,
-    req_id integer auto_increment,
+    req_id integer primary key autoincrement,
     med_facility_id integer,
-    primary key (req_id),
     foreign key (med_facility_id) references medical_facilities(med_facility_id),
     foreign key (blood_type) references blood_bank(blood_type)
 );
@@ -49,7 +46,6 @@ create table requests (
 
 create table medical_facilities (
     med_facility_name string,
-    med_facility_id integer auto increment,
-    validated boolean,
-    primary key (med_facility_id)
+    med_facility_id integer primary key autoincrement,
+    validated boolean
 );
