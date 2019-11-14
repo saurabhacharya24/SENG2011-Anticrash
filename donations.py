47e07525cdae03ace6257665a1867d8806e0d6c9
Shilpa <z5170043@ad.unsw.edu.au>
2019-11-15 01:53:23 +1100

    some donation class functions

---
+++ donations.py
@@ -0,0 +1,69 @@
+import sqlite3
+from datetime import timedelta, date
+import random
+
+class Donations:
+
+    def __init__(self, donor_id:int, blood_type:str, location_of_donation:str, blood_amount:int):
+        self.donor_id = donor_id
+        self.blood_type = blood_type
+        self.location_of_donation = location_of_donation
+        self.use_by_date = date.today() + timedelta(days=42) #unsure about date.today()
+        self.abnormalities = False
+        self.blood_amount = blood_amount
+        self.added_to_bank = False
+
+    #accepts donation as a sample only (not to bloodbank)
+    def accept_donation(self):
+        valid_donor = self.validate_donor_ID(self.donor_id)
+        valid_blood_amt = self.validate_blood_amt(self.blood_amount)
+        if valid_donor and valid_blood_amt:
+            #test blood for abnormalities
+            self.test_blood()
+            sql_insert_sample = """insert into donor_samples(donor_id, blood_type, location_of_donation, use_by_date, abnormalities, blood_amount, added_to_bank) values (?, ?, ?, ?, ?, ?, ?)"""
+            print(self.donor_id, self.blood_type, self.location_of_donation, self.use_by_date, self.abnormalities, self.blood_amount, self.added_to_bank)       
+            conn = self.connect_to_db()
+            cur = conn.cursor()
+            cur.execute(sql_insert_sample, (self.donor_id, self.blood_type, self.location_of_donation, self.use_by_date, self.abnormalities, self.blood_amount, self.added_to_bank))
+            conn.commit()            
+        else:
+            print("Not a registered donor")
+
+    def validate_donor_ID(self, donor_id):
+        conn = self.connect_to_db()
+        cur = conn.cursor()
+        cur.execute(f"select * from donors where donor_id={donor_id};")
+        #assumption that id's are unique
+        row = cur.fetchall()
+        if(row):
+            return True
+        else:
+            return False
+
+    def validate_blood_amt(self, blood_amount):
+        if 450 <= blood_amount <= 550:
+            return True
+        else:
+            return False
+
+    def test_blood(self):
+        test = random.randint(0, 100)
+        if(test > 95):
+            self.abnormalities = True
+        else:
+            self.abnormalities = False
+
+    # Helper method to connect to db
+    def connect_to_db(self):
+        conn = sqlite3.connect("database/anticrash.db")
+        return conn
+
+    # Helper method to close db
+    def disconnect_db(self, conn):
+        conn.close()
+
+
+test = Donations(1, "A", "15 Alice St", 500)
+# a = test.validate_donor_ID(150)
+# print(a)
+a = test.accept_donation()
